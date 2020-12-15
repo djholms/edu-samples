6691917cd140695f5c4e43071487ae9eacec1ddd
Vitalii Dieiev <v.dieiev@weplay.tv>
2020-12-15 13:16:06 +0200

    Added 4 examples

---
+++ python/ZedShaw_LPHW/ex2/ex2.py
@@ -0,0 +1,4 @@
+# A comment
+print("I could have code like this") # and the comment
+# print("This won't run")
+print("This will run")
---
+++ python/ZedShaw_LPHW/ex3/ex3_1.py
@@ -0,0 +1,22 @@
+print("I'll now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100.0 - 25.0 * 3 % 4)
+
+print("Now I'll count the eggs:")
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3+2<5-7?")
+
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2?", 3+2)
+print("What is 5 - 7?", 5-7)
+
+print("Oh, that's why it's False")
+
+print("How abou some more")
+
+print("Is it grather?", 5 > -2)
+print("Is it grather or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
